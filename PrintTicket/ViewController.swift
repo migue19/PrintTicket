133a5f3563619be124336d2d909b0b8e21dc1cdc
Miguel Mexicano <miguel.mexicano@hey.inc>
2023-07-28 18:25:45 -0600

    add code for printing ticket

--- PrintTicket/ViewController.swift
+++ PrintTicket/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
-
+import Printer
 class ViewController: UIViewController {
-
+    private let bluetoothPrinterManager = BluetoothPrinterManager()
+    private let dummyPrinter = DummyPrinter()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func printTicket(_ sender: Any) {
+        guard let image = UIImage(named: "ic_logo") else {
+            return
+        }
+        let date = Date().description
+        let ticket = Ticket(
+            .title("WorkLine"),
+            .text(.init(content: "Bienvenido", predefined: .bold, .alignment(.center))),
+            .image(image, attributes: .alignment(.center)),
+            .text(.init(content: date, predefined: .alignment(.center))),
+            .dividing(),
+            Block(Text(content: "Gracias por tu preferencia", predefined: .alignment(.center))),
+            .qr(date)
+        )
+        if bluetoothPrinterManager.canPrint {
+            bluetoothPrinterManager.print(ticket)
+        } else {
+            let printers = bluetoothPrinterManager.nearbyPrinters
+            for printer in printers {
+                bluetoothPrinterManager.connect(printer)
+                break
+            }
+        }
+    }
 }
-
